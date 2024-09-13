6500c4e1000933a339a09fc3121327d2ecc0bf86
v_hanwma <v_hanwma@tencent.com>
2024-09-13 17:32:25 +0800

    fix: something

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
-import { ref, onMounted, onBeforeUnmount, nextTick, watch, reactive, computed } from "vue";
+import {
+  ref,
+  onMounted,
+  onBeforeUnmount,
+  nextTick,
+  watch,
+  reactive,
+  computed,
+} from "vue";
 import type { Ref } from "vue";
 import { ElMessageBox } from "element-plus";
 import { debounce } from "lodash";
-import useVirtualList from './useVirtualList';
+import useVirtualList from "./useVirtualList";
 
 const tableData: Ref<any[]> = ref([]);
 
@@ -25,6 +33,7 @@ onMounted(() => {
     let i = 1;
     while (remoteData.length < max) {
       remoteData.push({
+        id: i,
         date: "2016-05-03_" + i,
         name: "Tom_" + i,
         address: "No. 189, Grove St, Los Angeles _ " + i,
@@ -36,11 +45,12 @@ onMounted(() => {
 
 const { actualRenderData } = useVirtualList({
   data: tableData, // 列表项数据
-  itemHeight: 67,
+  itemHeight: 51,
   size: 10,
-  scrollContainer: '.el-scrollbar__wrap', // 滚动容器
-  itemsWrapContainer: '.el-scrollbar__view', // 列表项父容器
-  marginContainer: '.el-table__body',// 需要偏移的目标元素
+  scrollContainer: ".el-scrollbar__wrap", // 滚动容器
+  actualHeightContainer: ".el-scrollbar__view", // 渲染实际高度的容器
+  tranlateContainer: ".el-table__body", // 需要偏移的目标元素,
+  itmeContainer: '.item'
 });
 
 const dialogVisible = ref(false);
@@ -53,6 +63,10 @@ const clickAct = (row) => {
   dialogVisible.value = true;
   rowData = row;
 };
+
+const rowClassName = (r) => {
+  return `row-${r.rowIndex}`
+}
 </script>
 
 <template>
@@ -64,7 +78,25 @@ const clickAct = (row) => {
 
   <h2>Total: {{ tableData.length }}</h2>
 
-  <el-table
+  <ul
+    class="el-scrollbar__wrap"
+    style="height: 500px; overflow-y: auto; border: 1px solid"
+  >
+    <div class="el-scrollbar__view">
+      <div class="el-table__body">
+        <li
+          class="item"
+          :key="item.name"
+          v-for="(item, i) in actualRenderData"
+          :style="`height: ${i == 0 ? 30 : i * 30}px; text-align: center; border-bottom: 1px solid red`"
+        >
+          {{ item.name }}
+        </li>
+      </div>
+    </div>
+  </ul>
+
+  <!-- <el-table
     :data="actualRenderData"
     stripe
     style="width: 100%"
@@ -76,7 +108,7 @@ const clickAct = (row) => {
     <el-table-column prop="date" label="Date" width="180" />
     <el-table-column prop="name" label="Name" width="180" />
     <el-table-column prop="address" label="Address" />
-  </el-table>
+  </el-table> -->
 
   <el-dialog
     v-model="dialogVisible"
@@ -97,16 +129,4 @@ const clickAct = (row) => {
 </template>
 
 <style>
-.el-table__row {
-  cursor: pointer;
-}
-.el-table__body .el-table__row > td {
-  padding: 0 !important;
-  height: 52px !important;
-}
-.el-table__body,
-.el-table__footer,
-.el-table__header {
-  table-layout: auto !important;
-}
 </style>
--- src/useVirtualList copy.ts
+++ src/useVirtualList copy.ts
@@ -4,7 +4,6 @@ import {
   onBeforeUnmount,
   watch,
   computed,
-  nextTick,
 } from "vue";
 
 import type { Ref } from "vue";
@@ -16,7 +15,6 @@ interface Config {
   scrollContainer: string;// 滚动容器的元素选择器
   actualHeightContainer: string;// 用于撑开高度的元素选择器
   tranlateContainer: string;// 用于偏移的元素选择器
-  itmeContainer: string;
 }
 
 type HtmlElType = HTMLElement | null;
@@ -39,22 +37,9 @@ export default function useVirtualList(config: Config) {
   });
 
   // 实际渲染的数据
-  const RenderedItemsHeightMap:any = {};
   const startIndex = ref(0);
   const endIndex = ref(config.size - 1);
   const actualRenderData = computed(() => {
-    nextTick(() => {
-      const Items: HTMLElement[] = Array.from(document.querySelectorAll(config.itmeContainer));
-      Items.forEach((el) => {
-        const key = el.innerText;
-        if(!RenderedItemsHeightMap[key]) {
-          RenderedItemsHeightMap[key] = el.clientHeight;
-        };
-      });
-      console.log(RenderedItemsHeightMap);
-
-      // actualHeightContainerEl!.style.height = 
-    });
     return config.data.value.slice(startIndex.value, endIndex.value + 1);
   });
 
@@ -62,6 +47,14 @@ export default function useVirtualList(config: Config) {
   const handleScroll = (e) => {
     const target = e.target;
     const { scrollTop, clientHeight, scrollHeight } = target;
+
+    // 边界控制：实际触底，且页面正常渲染全部数据时，不再触发后续计算，防止触底抖动
+    if (
+      scrollHeight <= scrollTop + clientHeight &&
+      endIndex.value >= config.data.value.length
+    ) {
+      return;
+    }
 
     // 保证数据渲染一直在可视区
     tranlateContainerEl!.style.transform = `translateY(${scrollTop}px)`;
